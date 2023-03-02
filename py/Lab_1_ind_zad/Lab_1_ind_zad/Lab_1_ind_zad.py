03e25dd34da646ad15d87091f8b2838f4bb54d35
Alexey Dorofeev <ppapa9141@gmail.com>
2023-03-02 11:38:03 +0300

    the code generates a list of elements randomly from the range from 5 to 4* 100 (the number of elements is 4 + 10)

---
+++ py/Lab_1_ind_zad/Lab_1_ind_zad/Lab_1_ind_zad.py
@@ -0,0 +1,4 @@
+#4 номер по журналу
+import random
+for i in range(0,14):
+    print(random.randint(5,4*100))
---
+++ py/Laba_1/Laba_1/Laba_1.py
@@ -0,0 +1,5 @@
+print("Hello Python from Visual Studio!")
+s = "a" * 30
+print(s)
+print("Newproject")
+print(s)
