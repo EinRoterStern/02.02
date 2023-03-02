0d8fdfdac3745a1866c2c05c7f017db6a03f22b8
Alexey Dorofeev <ppapa9141@gmail.com>
2023-03-02 12:03:13 +0300

    code as a function, calling it with the specified number of elements

--- py/Lab_1_ind_zad/Lab_1_ind_zad/Lab_1_ind_zad.py
+++ py/Lab_1_ind_zad/Lab_1_ind_zad/Lab_1_ind_zad.py
@@ -1,4 +1,9 @@
 #4 номер по журналу
+# -*- coding: cp1251 -*-    #кодировка
 import random
-for i in range(0,14):
-    print(random.randint(5,4*100))
+n=int(input("Введите число элементов - "))
+def function(x):
+    for i in range(x):
+        a = print(random.randint(5,4*100))
+    return a
+function(n)
